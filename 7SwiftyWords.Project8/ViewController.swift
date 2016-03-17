a5223c4e3c13fbc825ea5c9267498f284c5db7a7
ElijahButers <Elijahbuters@gmail.com>
2016-03-17 19:57:45 +0200

    Added func loadLevel(), where code loads and parses level text file, randomly assigns letter groups to buttons.

--- 7SwiftyWords.Project8/ViewController.swift
+++ 7SwiftyWords.Project8/ViewController.swift
@@ -39,6 +39,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func loadLevel() {
+        
+        var clueString = ""
+        var solutionString = ""
+        var letterBits = [String]()
+        
+        if let levelFilePath = NSBundle.mainBundle().pathForResource("level\(level)", ofType: "txt") {
+            
+            if let levelContents = try? String(contentsOfFile: levelFilePath, usedEncoding: nil) {
+                var lines = levelContents.componentsSeparatedByString("\n")
+                
+                lines = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(lines) as! [String]
+                
+                for (index, line) in lines.enumerate() {
+                    let parts = line.componentsSeparatedByString(": ")
+                    let answer = parts[0]
+                    let clue = parts[1]
+                    
+                    clueString += "\(index + 1). \(clue)\n"
+                    
+                    let solutionWord = answer.stringByReplacingOccurrencesOfString("|", withString: "")
+                    solutionString += "\(solutionWord.characters.count) letters\n"
+                    solutions.append(solutionWord)
+                    
+                    let bits = answer.componentsSeparatedByString("|")
+                    letterBits += bits
+                }
+            }
+        }
+        
+        // Configure the buttons and labels
+    }
+    
     // Actions
 
     @IBAction func submitTapped(sender: AnyObject) {
